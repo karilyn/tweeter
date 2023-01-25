93dadbd5f9b1da2b40fef529575c4660475dac89
karilyn <karilyn.kempton@gmail.com>
2023-01-24 21:16:38 -0800

    Add createTweetElement and renderTweets functions The createTweetElement function takes in a tweet object and returns a full article.tweet-container. The renderTweets function calls the createTweetElement function and then renders those tweets dynamically on the index page.

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,77 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+
+
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+]
+
+
+const createTweetElement = function (tweetData) {
+  let $tweet = $(`
+    <article class="tweet-container">
+      <header>
+        <ul class="name-list">
+          <li class="name">
+            <img src=${tweetData.user.avatars}">
+            ${tweetData.user.name}
+          </li>
+          <li class="handle">${tweetData.user.handle}</li>
+        </ul>
+      </header>
+      <section class="tweet">
+        <p>${tweetData.content.text}</p>
+        <div class="h_line"></div>
+      </section>
+      <footer>
+        <section class="ago">
+          <small>${Math.floor((Number(new Date()) - tweetData.created_at) / 60 / 60 / 24)} days ago</small>
+        </section>
+        <section class="icons">
+        <i class="icons fa-solid fa-flag"></i>
+        <i class="icons fa-sharp fa-solid fa-retweet"></i>
+        <i class="icons fa-solid fa-heart"></i>
+        </section>
+      </footer>
+    </article>`
+  );
+  return $tweet;
+};
+
+const renderTweets = function (data) {
+  // loops through tweets
+  $('.tweet-container').remove();
+  data.forEach((tweet) => {
+    // calls createTweetElement for each tweet
+    const $tweet = createTweetElement(tweet);
+    $('.container').append($tweet);
+  });
+};
+
+
+$(function(){
+  renderTweets(data);
+});
+
