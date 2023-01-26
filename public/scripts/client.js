406f674faebad4a1be2a007cc1a78f2e984b616d
karilyn <karilyn.kempton@gmail.com>
2023-01-25 17:09:01 -0800

    Add loadTweets function so that renderTweets can be called

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -6,30 +6,30 @@
 
 
 
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-];
+// const data = [
+//   {
+//     "user": {
+//       "name": "Newton",
+//       "avatars": "https://i.imgur.com/73hZDYK.png"
+//       ,
+//       "handle": "@SirIsaac"
+//     },
+//     "content": {
+//       "text": "If I have seen further it is by standing on the shoulders of giants"
+//     },
+//     "created_at": 1461116232227
+//   },
+//   {
+//     "user": {
+//       "name": "Descartes",
+//       "avatars": "https://i.imgur.com/nlhLi3I.png",
+//       "handle": "@rd" },
+//     "content": {
+//       "text": "Je pense , donc je suis"
+//     },
+//     "created_at": 1461113959088
+//   }
+// ];
 
 $( document ).ready(function() {
 
@@ -75,7 +75,7 @@ $( document ).ready(function() {
   };
 
 
-  renderTweets(data);
+  // renderTweets(data);
 
 
   const $form = $('#form');
@@ -86,6 +86,14 @@ $( document ).ready(function() {
     const URL = "/tweets"
     $.post(URL, $input);
   });
+
+
+  const loadTweets = function() {
+    $.ajax('/tweets', { method: 'GET' })
+    .then(renderTweets(data).serialize());
+  };
+
+  loadTweets();
 
 
 
